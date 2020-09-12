3fb2f94eb01f47c8905061da371c1137b7ae3743
Abhishek <abhishek.v.gaikwad@gmail.com>
2020-09-12 14:25:32 +0530

    Updating shop page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
-import { withRouter } from 'react-router-dom';
-import {Switch, Router} from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.component'
+import {Switch, Route} from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div >
-      <HomePage />
+      <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop" component={ShopPage} />
+      
+      </Switch>
     </div>
   );
 }
