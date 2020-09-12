4d518067866853f1bd6036ab4fa26c341c67217b
Abhishek <abhishek.v.gaikwad@gmail.com>
2020-09-12 12:33:12 +0530

    update our menu-items to nagivate withRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import { withRouter } from 'react-router-dom';
+import {Switch, Router} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
 
 function App() {
