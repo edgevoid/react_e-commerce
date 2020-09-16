ed7540ea342129c3f13bf685806cf6df84dbab24
Abhishek <abhishek.v.gaikwad@gmail.com>
2020-09-16 09:24:27 +0530

    added dispatcher to cart-item component and updated reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,6 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+const store = createStore(rootReducer, applyMiddleware(...middlewares) );
 
 export default store;
